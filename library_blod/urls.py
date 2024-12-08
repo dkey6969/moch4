68061e6fa0e0603addce0f54effc9c97be9a3723
dkey6969 <heromamasky@>
2024-12-08 21:35:46 +0600

    added homework

--- library_blod/urls.py
+++ library_blod/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list_view, name='book_list'),
-    path('books_detail/<int:id>/', views.book_detail_view, name='book_detail'),
+    path('book_detail/<int:id>/', views.book_detail_view, name='book_detail'),
     path('about_me/', views.about_me, name='about_me'),
     path('about_pets/', views.about_pets, name='about_pets'),
     path('system_time/', views.system_time, name='system_time'),
